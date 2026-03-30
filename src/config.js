0ca753482f32fc2789e9ae3c5c5322b753380945
Alfonso Palacios <alfpala@gmail.com>
2026-03-29 23:06:27 -0500

    Update API_BASE_URL to include '/api/'

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 // src/config.js
 
-export const API_BASE_URL = "https://proyecto-subir-git-final-backend.onrender.com/";
+export const API_BASE_URL = "https://proyecto-subir-git-final-backend.onrender.com/api/";
